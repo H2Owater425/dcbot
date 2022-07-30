d4260f0a1cb1348ef7250e6fd9f0c578e37b27dc
H2Owater425 <h2o@h2owr.xyz>
2022-07-30 20:22:11 +0900

    feat: Implement sharding

--- source/application.ts
+++ source/application.ts
@@ -1,18 +1,16 @@
 import '@library/environment';
 import { Client } from '@library/framework';
 import { join } from 'path';
+import { ClientOptions } from 'eris';
 
-export const client: Client = new Client(process['env']['TOKEN'], {
+export const client: Client = new Client(process['env']['TOKEN'], Object.assign({
 	intents: ['guilds', 'guildEmojisAndStickers', 'guildMessages', 'guildMessageReactions'],
 	prefix: process['env']['PREFIX'],
-	defaultHelpCommand: true,
-	restMode: true
-});
+	defaultHelpCommand: true
+}, typeof(process['env']['MAXIMUM_SHARD']) === 'string' && /^[1-9][0-9]*$/.test(process['env']['MAXIMUM_SHARD']) ? { maxShards: Number.parseInt(process['env']['MAXIMUM_SHARD'], 10) } : undefined) as ClientOptions);
 
 client.loadCommand(join(__dirname, 'command'));
 
 client.loadEvent(join(__dirname, 'event'));
 
 client.connect();
-
-// TODO: I think we can just remove id from tabel
---
+++ source/command/ping.ts
@@ -0,0 +1,16 @@
+import { client } from "@application";
+import { Command } from "@library/framework";
+import logger from "@library/logger";
+import { Message, Shard } from "eris";
+
+export default new Command('!ping', function (message: Message): void {
+	const latency: number = (client['shards'].get(client['guildShardMap'][message['guildID'] as string]) as Shard)['latency'];
+
+	message['channel'].createMessage({ content: Number.isFinite(latency) ? 'pong (' + latency + 'ms)' : 'Please try again later' })
+	.catch(logger.error);
+
+	return;
+}, {
+	aliases: ['!핑'],
+	guildOnly: true
+});
